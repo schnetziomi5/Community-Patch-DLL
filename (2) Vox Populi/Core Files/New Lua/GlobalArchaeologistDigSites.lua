3acf15df546bddafafe7ef79efab3ebb2586772d
Daniel <danielk1709@gmail.com>
2025-12-14 23:08:38 +0100

    Adjust files in (2) and (4)

--- (2) Vox Populi/Core Files/New Lua/GlobalArchaeologistDigSites.lua
+++ (2) Vox Populi/Core Files/New Lua/GlobalArchaeologistDigSites.lua
@@ -1,3 +1,4 @@
+if CodeBuddy.vpRegisterContext and not CodeBuddy.vpRegisterContext() then return end
 print("This is the 'Global - Archaeologist Dig Sites' mod script.")
 
 local iArchaeologist = GameInfoTypes.UNIT_ARCHAEOLOGIST
--- (2) Vox Populi/LUA/CityBannerManager.lua
+++ (2) Vox Populi/LUA/CityBannerManager.lua
@@ -1,3 +1,4 @@
+if CodeBuddy.vpRegisterContext and not CodeBuddy.vpRegisterContext() then return end
 print("This is the modded CityBannerManager from CBP")
 
 -------------------------------------------------
