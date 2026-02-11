8bd35f89b8928751005ca817d510b26a090a6f9a
Daniel <danielk1709@gmail.com>
2026-02-11 13:21:39 +0100

    fix

--- CvGameCoreDLL_Expansion2/Lua/CvLuaUnit.cpp
+++ CvGameCoreDLL_Expansion2/Lua/CvLuaUnit.cpp
@@ -1629,8 +1629,8 @@ int CvLuaUnit::lCanEverRangeStrikeAt(lua_State* L)
 	CvUnit* pkUnit = GetInstance(L);
 	const int x = lua_tointeger(L, 2);
 	const int y = lua_tointeger(L, 3);
-	
-	if ( lua_gettop >= 5 )
+
+	if ( lua_gettop(L) >= 5 )
 	{
 		const int sx = lua_tointeger(L, 4);
 		const int sy = lua_tointeger(L, 5);
