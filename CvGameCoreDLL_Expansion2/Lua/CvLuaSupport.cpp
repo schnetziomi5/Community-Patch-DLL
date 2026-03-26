c2a01af719df64ffa0b99506f0fe42a3c7ac2d05
Daniel <danielk1709@gmail.com>
2026-03-26 22:19:51 +0100

    commit script

--- CvGameCoreDLL_Expansion2/Lua/CvLuaSupport.cpp
+++ CvGameCoreDLL_Expansion2/Lua/CvLuaSupport.cpp
@@ -61,57 +61,248 @@ void LuaSupport::RegisterScriptData(lua_State* L)
 	CvLuaPlayer::Register(L);
 	CvLuaTeam::Register(L);
 
+	//Compat for older mods:
+	//Additionally put Player and Team directly into the gamecore table again
+	lua_getglobal(L,"LuaTypes");
+	lua_getfield(L,-1,"Team");
+	lua_setglobal(L,"Team");
+	lua_getfield(L,-1,"Player");
+	lua_setglobal(L,"Player");
+	lua_pop();
+
 	//Register additional lua types, for which instances will only be created on demand
 	CvLuaCity::PushTypeTable(L);
 	CvLuaPlot::PushTypeTable(L);
 	CvLuaUnit::PushTypeTable(L);
 	CvLuaTeamTech::PushTypeTable(L);
-
-	/*
-	//-----------------------------------------
-	// Restore access to some core lua functionality hidden away by Fireaxis, but still keep the sandbox intact
-	// Use lua_dostring(), because it's more comfortable to code and easier to see what's happening
-
-	lua_pushvalue(L, LUA_REGISTRYINDEX);
-	lua_setglobal(L, "R");
-
-	const char* luacommand = "" 
-
-	"local G = R._LOADED._G;"
-	"R = nil;"
-
-	"rawset = G.rawset;"
-	"rawget = G.rawget;"
-	"newproxy = G.newproxy;"
-	"if G.jit then LUAJIT = true; bitops = G.require('bit') end;"
-
-	"FireaxisObjects={"
-		"UI = G.UI;"
-		"Threads = G.Threads;"
-		"GameInfo = G.GameInfo;"
-		"GameDefines = G.GameDefines;"
-	"};"
-
-	"local g = G.getfenv;"
-	"GameCore = g(0);"
-	"_ENV = G.setmetatable( {}, {"
-		"__index = function(T,k) return g(0)[k] end;"
-		"__newindex = function(T,k,v) g(0)[k] = v end;"
-		"__call = function() return g(0) end;"
-	"});";
-	
-	luaL_dostring(L,luacommand);
-
-	//------------------------------------------
-	*/
-
+	CvLuaDeal::PushTypeTable(L);
+	CvLuaArea::PushTypeTable(L);
+	CvLuaLeague::PushTypeTable(L);
 }
 
 void LuaSupport::InitLuaFramework()
 {
 	ICvEngineScriptSystem1* pkScriptSystem = gDLL->GetScriptSystem();
-	lua_State* L = pkScriptSystem->CreateLuaThread("VP_TMP");
-	pkScriptSystem->LoadFile(L, "VP_LuaFramework.lua");
+	lua_State* L = pkScriptSystem->CreateLuaThread("VP_LUAAPI");
+
+	lua_pushvalue(L,LUA_REGISTRYINDEX);
+	lua_setglobal(L,"REGISTRY");
+
+	const char* luaCommand = ""
+
+"local G = REGISTRY.G ;\n"
+"local Threads = G.Threads ;\n"
+"local IncludeFileList = G.IncludeFileList ;\n"
+"local newproxy = G.newproxy ;\n"
+"local setfenv = G.setfenv ;\n"
+"local getfenv = G.getfenv ;\n"
+"local rawset = G.rawset ;\n"
+"local rawget = G.rawget ;\n"
+"local pcall = pcall ;\n"
+"local string_find = string.find ;\n"
+"local print = print ;\n"
+"local _ENV = getfenv(0) ;\n"
+"local coroutine = coroutine ;\n"
+"\n"
+"Threads[coroutine.running()] = {} ; -- Prevent my global env from being cleared out by the engine!\n"
+"\n"
+"local function sandboxedCall( func, ... ) setfenv( 0, getfenv(0), setfenv( 0, getfenv(func) ), pcall( func, ... ) ) end;\n"
+"local newThreadCallback = nil ;\n"
+"local function setNewThreadCallback( func ) newThreadCallback = ( type(func) == 'function' and func ) or error('Not a function!') end;\n"
+"\n"
+"local function getThreadEnvByName( name )\n"
+"	for k,v in pairs(Threads) do\n"
+"		if v.StateName == name then\n"
+"			return v ;\n"
+"		end\n"
+"	end\n"
+"end;\n"
+"\n"
+"local doesIncludeExist = function( file )\n"
+"	local length = #file ;\n"
+"	for _,val in ipairs(IncludeFileList) do\n"
+"		if string_find(val,file,length) then\n"
+"			return val ;\n"
+"		end\n"
+"	end\n"
+"end;\n"
+"\n"
+"local function copyTableEntries( to, from )\n"
+"	for k,v in pairs(from) do \n"
+"		to[k] = v ;\n"
+"	end\n"
+"end;\n"
+"\n"
+"local extraApi = {\n"
+"	setNewThreadCallback = setNewThreadCallback ;\n"
+"	rawset = rawset ;\n"
+"	rawget = rawget ;\n"
+"	newproxy = newproxy ;\n"
+"	settenv = function( env,... ) local ret = getfenv(0); setfenv(0, env); return ret,... end ;\n"
+"	gettenv = function() return getfenv(0) end ;\n"
+"	doesIncludeExist = doesIncludeExist ;\n"
+"	firaxisGameInfo = G.GameInfo ;\n"
+"};\n"
+"\n"
+"if G.jit then\n"
+"	extraApi.jit = true ;\n"
+"	extraApi.bit = G.require('bit');\n"
+"end\n"
+"\n"
+"local baseApi = nil ;\n"
+"local loaderCoroutine = coroutine.create( function( env )\n"
+"	while true do\n"
+"		print('################################################################################');\n"
+"		print('(Re-)Loading api context!');\n"
+"		setfenv(0,env);\n"
+"		print('<-Switching context! ContextPtr:', env.ContextPtr);\n"
+"		if env.VPUI_loader then\n"
+"			print('Releasing previous api context! ContextPtr:', env.VPUI_loader);\n"
+"			env.ContextPtr:ReleaseChild(env.VPUI_loader);\n"
+"		end\n"
+"		env.VPUI_loader = env.ContextPtr:LoadNewContext('VPUI_loader');\n"
+"		setfenv(0,_ENV);\n"
+"		print('New api context created! ContextPtr:' , env.VPUI_loader);\n"
+"		print('################################################################################');\n"
+"		env = coroutine.yield();\n"
+"	end\n"
+"end);\n"
+"\n"
+"local function getBaseApi( reload ) \n"
+"	if reload or (not baseApi) then\n"
+"		newThreadCallback = nil ;\n"
+"		local ttenv = getThreadEnvByName('ToolTips') ;\n"
+"		baseApi = { quicktraceback = ttenv.quicktraceback } ;\n"
+"		coroutine.resume( loaderCoroutine , ttenv );\n"
+"	end\n"
+"	return baseApi;\n"
+"end;\n"
+"\n"
+"local GAMECORE_DELETE_NEXT_CHECK = false ;\n"
+"local function checkDeleteGamecore( delete_next_time )\n"
+"	if GAMECORE_DELETE_NEXT_CHECK then\n"
+"		G.GameCore = nil ;\n"
+"	end\n"
+"	GAMECORE_DELETE_NEXT_CHECK = delete_next_time ;\n"
+"	baseApi = nil ;\n"
+"	newThreadCallback = nil ;\n"
+"end;\n"
+"\n"
+"local function pushApi( to )\n"
+"	copyTableEntries( to, getBaseApi() );\n"
+"	to._ENV = to ;\n"
+"	if newThreadCallback then sandboxedCall( newThreadCallback, to ) end;\n"
+"end;\n"
+"\n"
+"local IMPORT_BLACKLIST = {\n"
+"	StateName = true ;\n"
+"	Events = true ;\n"
+"	LuaEvents = true ;\n"
+"	GameEvents = true ;\n"
+"	Controls = true ;\n"
+"	ContextPtr = true ;\n"
+"};\n"
+"\n"
+"local registrationListenerVPUILoader = { __newindex = function( to, key, value )\n"
+"	if key == 'PreGame' then\n"
+"		setmetatable(to,nil);\n"
+"		copyTableEntries(to, extraApi);\n"
+"		to.exportedObjects = baseApi ;\n"
+"		to.GameCore = G.GameCore ;\n"
+"		to._ENV = to ;\n"
+"	end\n"
+"	rawset(to, key, value);\n"
+"	if not IMPORT_BLACKLIST[key] then \n"
+"		baseApi[key] = value;\n"
+"	end\n"
+"end;};\n"
+"\n"
+"local registrationListenerFinishBeforeControls = { __newindex = function( to, key, value )\n"
+"	if key == 'Controls' and not(G.GameCore and G.GameCore.Controls == value) then\n"
+"		setmetatable(to,nil);\n"
+"		pushApi(to);\n"
+"	end\n"
+"	rawset(to, key, value);\n"
+"end;};\n"
+"\n"
+"local registrationListenerWaitForPreGame = { __newindex = function( to, key, value )\n"
+"	if key == 'PreGame' then\n"
+"		setmetatable(to, registrationListenerFinishBeforeControls);\n"
+"	end\n"
+"end;};\n"
+"\n"
+"local registrationListenerFinishAfterArtInfo = { __newindex = function( to, key, value )\n"
+"	if key == 'ArtInfo' and not(G.GameCore and G.GameCore.ArtInfo == value) then\n"
+"		setmetatable(to,nil);\n"
+"		pushApi(to);\n"
+"	else\n"
+"		rawset(to, key, value);\n"
+"	end\n"
+"end;};\n"
+"\n"
+"local registrationListenerPotentiallyMatchmakingLib = { __newindex = function( to, key, value )\n"
+"	if key == 'Matchmaking' and not(G.GameCore and G.GameCore.Matchmaking == value) then\n"
+"		setmetatable(to, registrationListenerWaitForPreGame);\n"
+"	else\n"
+"		setmetatable(to, registrationListenerFinishAfterArtInfo);\n"
+"	end\n"
+"end;};\n"
+"\n"
+"local registrationListenerWaitForEvents = { __newindex = function( to, key, value )\n"
+"	if key == 'LuaEvents' or key == 'Events' then\n"
+"		rawset(to, key, value);\n"
+"	elseif key == 'UI' then\n"
+"		setmetatable(to, registrationListenerPotentiallyMatchmakingLib);\n"
+"	end\n"
+"end;};\n"
+"\n"
+"local registrationListenerCheckoutType = { __newindex = function( to, key, value )\n"
+"	local stn = to.StateName ;\n"
+"	setmetatable(to,nil);\n"
+"	if stn == nil then -- ParseMapScript or GameCore creation - Have yet to figure out how to handle them efficiently\n"
+"		print('VP_LUAAPI: Ignoring exe thread!');\n"
+"		rawset(to, key, value);\n"
+"	elseif stn == 'VP_LUAAPI' then\n"
+"		print('VP_LUAAPI: Ignoring itself!');\n"
+"		newThreadCallback = nil ;\n"
+"		baseApi = nil ;\n"
+"		rawset(to,key,value);\n"
+"	elseif stn == 'ToolTips' then\n"
+"		print('VP_LUAAPI: Ignoring TT thread!');\n"
+"		newThreadCallback = nil ;\n"
+"		baseApi = nil ;\n"
+"		rawset(to, key, value);\n"
+"	elseif stn == 'VPUI_loader' then\n"
+"		setmetatable(to, registrationListenerVPUILoader);\n"
+"		to[key]=value; --don't use rawset here!\n"
+"	else\n"
+"		if stn == 'Map Script' then\n"
+"			checkDeleteGamecore(true);\n"
+"		elseif stn == 'LoadScreen' then\n"
+"			checkDeleteGamecore(true);\n"
+"		end\n"
+"		setmetatable(to, registrationListenerWaitForEvents);\n"
+"	end\n"
+"end;};\n"
+"\n"
+"if getmetatable(Threads) then\n"
+"	print('Warning: Metatable for Threads already present!');\n"
+"end;\n"
+"\n"
+"setmetatable(Threads, { __newindex = function( to, thread, env )\n"
+"	rawset(to, thread, env);\n"
+"	if doesIncludeExist('VPUI_core.lua') then\n"
+"		setmetatable(env, registrationListenerCheckoutType);\n"
+"	else\n"
+"		print('VP_LUAAPI: Shutting down!');\n"
+"		setmetatable(to, nil);\n"
+"	end\n"
+"end;});\n"
+"\n"
+"print('Embedded script ran without errors!');\n";
+
+	luaL_dostring(L,luaCommand);
+	
 	pkScriptSystem->FreeLuaThread(L);
 }
 
